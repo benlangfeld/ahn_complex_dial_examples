548ce061dde3bbcaced6a05411f3b275fc2df33e
Ben Langfeld <ben@langfeld.me>
2013-08-20 13:52:40 -0300

    Use the version of dial_with_apps from Matrioska

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'adhearsion', github: 'adhearsion', branch: 'feature/complex_dial'
 gem 'punchblock', github: 'adhearsion/punchblock', branch: 'develop'
-gem 'matrioska', github: 'polysics/matrioska', branch: 'develop'
+gem 'matrioska', github: 'polysics/matrioska', branch: 'feature/dial_with_apps'
 
 group :development, :test do
   gem 'rspec'
--- config/adhearsion.rb
+++ config/adhearsion.rb
@@ -8,44 +8,7 @@ Adhearsion.config do |config|
   config.punchblock.password = "1"
 end
 
-# TODO: This should move to Matrioska
-module Matrioska::DialWithApps
-  private
-
-  def dial_with_local_apps(to, options = {}, &block)
-    dial = Adhearsion::CallController::Dial::ParallelConfirmationDial.new to, options, call
-
-    runner = Matrioska::AppRunner.new call
-    yield runner, dial
-    runner.start
-
-    dial.track_originating_call
-    dial.prep_calls
-    dial.place_calls
-    dial.await_completion
-    dial.cleanup_calls
-    dial.status
-  end
-
-  def dial_with_remote_apps(to, options = {}, &block)
-    dial = Adhearsion::CallController::Dial::ParallelConfirmationDial.new to, options, call
-
-    dial.track_originating_call
-
-    dial.prep_calls do |new_call|
-      new_call.on_joined call do
-        runner = Matrioska::AppRunner.new new_call
-        yield runner, dial
-        runner.start
-      end
-    end
-
-    dial.place_calls
-    dial.await_completion
-    dial.cleanup_calls
-    dial.status
-  end
-end
+require 'matrioska/dial_with_apps'
 
 class MidCallMenuController < Adhearsion::CallController
   include Matrioska::DialWithApps
